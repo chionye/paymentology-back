83cdb0e1897a0a0a545975f5d3433e56dde46f87
michael valentine <37175136+chionye@users.noreply.github.com>
2022-03-25 16:24:09 +0100

    Add files via upload

--- src/main/java/com/example/demo/service/Transactions.java
+++ src/main/java/com/example/demo/service/Transactions.java
@@ -11,35 +11,47 @@ public class Transactions {
     private static final List<String> contentTypes = List.of("text/csv", "application/vnd.ms-excel");
 
 
-    public HashMap<String, List<Map<String, String>>> generateTransactionRecordReport(List<Map<String, String>> row, List<Map<String, String>> row1, String name, String comparisonKey) {
+    public HashMap<String, List<Map<String, String>>> generateTransactionRecordReport(List<Map<String, String>> firstFile, List<Map<String, String>> secondFile, String filename1, String comparisonKey) {
         HashMap<String, List<Map<String, String>>> result = new HashMap<>();
         List<Map<String, String>> closeMatch = new ArrayList<>();
         List<Map<String, String>> matchedAgainst = new ArrayList<>();
         List<Map<String, String>> noMatch = new ArrayList<>();
-        for (int i = 0; i < row.size(); i++) {
-            for (int j = 0; j < row1.size(); j++) {
+        for (int i = 0; i < firstFile.size(); i++) {
+            for (int j = 0; j < secondFile.size(); j++) {
                 Map<String, String> matches;
-                if (row.get(i).get(comparisonKey).replace(" ", "").equals(row1.get(j).get(comparisonKey))) {
+                if (firstFile.get(i).get(comparisonKey).replace(" ", "").equals(secondFile.get(j).get(comparisonKey))) {
                     break;
-                } else if (row.size() - 1 == j && !(row1.get(i).get(comparisonKey).equals(row.get(j).get(comparisonKey)))) {
-                    matches = row1.get(i);
+                } else if (firstFile.size() - 1 == j && !(secondFile.get(i).get(comparisonKey).equals(firstFile.get(j).get(comparisonKey)))) {
+                    matches = secondFile.get(i);
                     noMatch.add(matches);
-                } else if (row.get(i).get(comparisonKey).regionMatches(0, row1.get(j).get(comparisonKey), 0, (row.get(i).get(comparisonKey).length()/2) + 2)) {
-                    System.out.print(row1.get(j).get(comparisonKey));
-                    matches = row.get(i);
-                    Map<String, String> closeMatchedTo = new HashMap<>(row1.get(j));
-                    closeMatch.add(matches);
-                    matchedAgainst.add(closeMatchedTo);
                 }
             }
         }
+
         result.put("noMatch", noMatch);
-        result.put("closeMatch", closeMatch);
+        result.put("closeMatch", closeMatch(noMatch, secondFile, comparisonKey));
         result.put("closeMatchedTo", matchedAgainst);
-        result.put("len", List.of(Map.of("size", String.valueOf(row.size()))));
-        result.put("name", List.of(Map.of("name", name)));
+        result.put("len", List.of(Map.of("size", String.valueOf(firstFile.size())), Map.of("size1", String.valueOf(secondFile.size()))));
+        result.put("name", List.of(Map.of("name", filename1)));
         return result;
     }
+
+    public List<Map<String, String>> closeMatch(List<Map<String, String>> noMatch, List<Map<String, String>> file, String key){
+        List<Map<String, String>> closeMatch = new ArrayList<>();
+        for (int i = 0; i < noMatch.size(); i++) {
+            for (int j = 0; j < file.size(); j++) {
+                if(noMatch.get(i).get(key).regionMatches(0, file.get(j).get(key), 0, (noMatch.get(i).get(key).length()/2) + 2) && !(noMatch.get(i).get(key).equals(file.get(j).get(key)))){
+                    closeMatch.add(file.get(j));
+                    break;
+                }
+                if (j == file.size() - 1 && !(noMatch.get(i).get(key).equals(file.get(j).get(key)))){
+                    closeMatch.add(Map.of());
+                }
+            }
+        }
+        return closeMatch;
+    }
+
 
     public List<String> getContentTypes() {
         return  contentTypes;
